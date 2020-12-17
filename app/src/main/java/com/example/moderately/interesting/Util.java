d5c7cfefc1aaabec57060f88708e10120b292287
Schotsl <sjorsvanholst@gmail.com>
2020-12-17 18:09:24 +0100

    🎨 Added draw helper function

--- app/src/main/java/com/example/moderately/interesting/Util.java
+++ app/src/main/java/com/example/moderately/interesting/Util.java
@@ -1,11 +1,22 @@
 package com.example.moderately.interesting;
 
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import com.example.moderately.interesting.properties.Position;
 
 public class Util {
     public static Bitmap resizeBitmap(int width, Bitmap bitmap) {
         double ratio = (double) bitmap.getHeight() / (double) bitmap.getWidth();
         double height = width * ratio;
+
         return Bitmap.createScaledBitmap(bitmap, width, (int) height, true);
+    }
+
+    public static void centerDraw(Position position, Bitmap bitmap, Canvas canvas) {
+        int xCenter = position.xPosition - bitmap.getWidth() / 2;
+        int yCenter = position.yPosition - bitmap.getHeight() / 2;
+
+        canvas.drawBitmap(bitmap, xCenter, yCenter, null);
     }
 }
